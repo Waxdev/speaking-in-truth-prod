f59442a8a0a1b1313db2a85f146241889838f494
Stéphane Montlouis-Calixte <smontlouis@kilix.fr>
2017-10-31 15:44:34 +0100

    Updates

---
+++ component---src-pages-404-js-cd51d374be13da4bdeeb.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x9427c64ab85d],{408:function(e,t,n){(function(u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=n(2),s=(l(a),function(){return u.createElement("div",null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))});t.default=s,e.exports=t.default}).call(t,n(15))}});
+//# sourceMappingURL=component---src-pages-404-js-cd51d374be13da4bdeeb.js.map
